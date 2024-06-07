e9dea5be10e61d7df2b70084afa453c6268f8d7f
ramkuamarji8756 <168971592+ramkuamarji8756@users.noreply.github.com>
2024-06-07 03:10:21 -0500

    Add files via upload

---
+++ src/main/java/com/ecom/service/UserService.java
@@ -0,0 +1,8 @@
+package com.ecom.service;
+
+import com.ecom.model.UserDtls;
+
+public interface UserService {
+
+	public UserDtls saveUser(UserDtls user);
+}
---
+++ src/main/java/com/ecom/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.ecom.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.ecom.model.UserDtls;
+import com.ecom.repository.UserRepository;
+import com.ecom.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	public UserDtls saveUser(UserDtls user) {
+		user.setRole("ROLE_USER");
+		String encodePassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodePassword);
+		UserDtls saveUser = userRepository.save(user);
+		return saveUser;
+	}
+}
